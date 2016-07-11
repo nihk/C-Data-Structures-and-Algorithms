976e84fcb69bf65205e7be0cdcf2b6143375a446
nihk <nickjrose@gmail.com>
2016-07-10 20:26:15 -0400

    Refactored driver

--- Algorithms/Searching/SearchingDriver.c
+++ Algorithms/Searching/SearchingDriver.c
@@ -3,6 +3,21 @@
 #include <string.h>
 #include "Searching.h"
 
+// Function prototypes
+void printBinarySearchResult(int result, int value);
+void searchAndPrintBinarySearchResult(int arr[], int len, int valueToFind);
+
+
+int main(void) {
+	const int len = 10;
+	int arr[] = { 2, 4, 7, 9, 13, 23, 45, 66, 89, 101 };
+
+	searchAndPrintBinarySearchResult(arr, len, 89);
+	searchAndPrintBinarySearchResult(arr, len, 12);
+}
+
+
+// Function definitions
 void printBinarySearchResult(int result, int value) {
 	if (result == -1) {
 		printf("%d was not found.\n", value);
@@ -14,12 +29,4 @@ void printBinarySearchResult(int result, int value) {
 void searchAndPrintBinarySearchResult(int arr[], int len, int valueToFind) {
 	int result = binarySearch(arr, len, valueToFind);
 	printBinarySearchResult(result, valueToFind);
-}
-
-int main(void) {
-	const int len = 10;
-	int arr[] = { 2, 4, 7, 9, 13, 23, 45, 66, 89, 101 };
-
-	searchAndPrintBinarySearchResult(arr, len, 89);
-	searchAndPrintBinarySearchResult(arr, len, 12);
 }
