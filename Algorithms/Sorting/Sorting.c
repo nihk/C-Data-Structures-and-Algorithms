a52a0eda4d2bb4010e64cda1fea3627dd879c79b
nihk <nickjrose@gmail.com>
2016-07-16 19:00:59 -0400

    Added two versions of quicksort

--- Algorithms/Sorting/Sorting.c
+++ Algorithms/Sorting/Sorting.c
@@ -69,8 +69,71 @@ void merge(int arr[], int low, int middle, int high) {
 	}
 }
 
+void quicksort1(int arr[], int low, int high) {
+	if (low < high) {
+		int divisionPoint = partition1(arr, low, high);
+		quicksort1(arr, low, divisionPoint - 1);
+		quicksort1(arr, divisionPoint + 1, high);
+	}
+}
+
+// pivot ends up in its final sorted position
+int partition1(int arr[], int low, int high) {
+	// random pivot to try and avoid worst case O(n^2)
+	swap(arr, low, random(low, high));
+	int pivot = arr[low];
+	int lastSmall = low;
+	for (int i = low + 1; i <= high; i++) {
+		if (arr[i] < pivot) {
+			lastSmall++;
+			swap(arr, i, lastSmall);
+		}
+	}
+	swap(arr, low, lastSmall);
+	return lastSmall;
+}
+
+void quicksort2(int arr[], int low, int high) {
+	if (low < high) {
+		int divisionPoint = partition2(arr, low, high);
+		quicksort2(arr, low, divisionPoint);
+		quicksort2(arr, divisionPoint + 1, high);
+	}
+}
+
+// pivot does not end up in its final sorted position
+int partition2(int arr[], int low, int high) {
+	// random pivot to try and avoid worst case O(n^2)
+	swap(arr, low, random(low, high));
+	int pivot = arr[low];
+	low--;
+	high++;
+
+	while (low < high) {
+		do {
+			high--;
+		} while (arr[high] > pivot);
+
+		do {
+			low++;
+		} while (arr[low] < pivot);
+
+		if (low < high) {
+			swap(arr, low, high);
+		}
+	}
+
+	return high;
+}
+
 void swap(int arr[], int i, int j) {
 	int temp = arr[i];
 	arr[i] = arr[j];
 	arr[j] = temp;
+}
+
+// returns a random i where m <= i <= n
+int random(int m, int n) {
+	int offset = rand()/(RAND_MAX + 1.0) * (n - m + 1);
+	return m + offset;
 }
